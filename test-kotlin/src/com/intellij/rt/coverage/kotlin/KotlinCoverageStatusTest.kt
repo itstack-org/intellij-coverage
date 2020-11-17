0d0a0e085cfe323af93a1df6d0fc6b477f878180
Maksim Zuev <maksim.zuev@jetbrains.com>
2020-11-17 23:04:42 +0300

    Fix: line mappings should be applied to one class

--- test-kotlin/src/com/intellij/rt/coverage/kotlin/KotlinCoverageStatusTest.kt
+++ test-kotlin/src/com/intellij/rt/coverage/kotlin/KotlinCoverageStatusTest.kt
@@ -81,6 +81,14 @@ class KotlinCoverageStatusTest {
     fun testReified() = test("inline.reified")
 
     @Test
+    fun testNoInline() = test("inline.noinline",
+            "TestKt", "TestKt\$test\$1", "TestKt\$test\$3")
+
+    @Test
+    fun testCrossInline() = test("inline.crossinline",
+            "TestKt", "TestKt\$test\$\$inlined\$save\$1", "TestKt\$save\$1")
+
+    @Test
     fun testMultiplyFilesInline() = test("inline.multiplyFiles", "Test2Kt",
             fileName = "test2.kt")
 
